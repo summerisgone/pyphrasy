02662c3ec2e9b75ddd93a25fc9d19097a35720cc
Ivan Gromov <summer.is.gone@gmail.com>
2013-09-19 00:40:31 +0600

    initial commit

---
+++ inflect.py
@@ -0,0 +1,143 @@
+# coding: utf8
+from operator import attrgetter
+import argparse
+import logging
+
+import pymorphy2
+
+
+LOGGING_LEVELS = {
+    0: logging.ERROR,
+    1: logging.INFO,
+    2: logging.DEBUG
+}
+logger = logging.getLogger()
+try:
+    import cStringIO as StringIO
+except ImportError:
+    import StringIO
+
+
+class InflectionError(Exception):
+    pass
+
+
+class PhraseInflector(object):
+
+    class ScoreHelper(object):
+        score = 0
+
+        def __init__(self, parsed):
+            self.parsed = parsed
+
+    def __init__(self, morph):
+        self.morph = morph
+
+    def parse_first(self, word):
+        parse = self.morph.parse(word)
+        if len(parse) > 0:
+            return parse[0]
+        else:
+            return None
+
+    def select_master(self, phrase):
+        versions = []
+        for i, word in enumerate(phrase):
+            for j, parsed in enumerate(self.morph.parse(word)):
+                if {'NOUN', 'nomn'} in parsed.tag:
+                    version = self.ScoreHelper(parsed)
+                    version.score = 100 / (2.0 * j + 1) + 100 / (1.0 * i + 1)
+                    versions.append(version)
+        if versions:
+            return sorted(versions, key=attrgetter('score'), reverse=True)[0]
+        else:
+            return None
+
+    def inflect(self, phrase, case):
+        # phrase = phrase.lower()
+        master_word = self.select_master(phrase.lower().split(' '))
+        if master_word:
+            return self._inflect_with_master(case, phrase, master_word.parsed)
+        else:
+            logger.error(u'Can not find master word in: {0}'.format(phrase))
+            return self._inflect_without_master(case, phrase)
+
+    def _inflect_with_master(self, case, phrase, master_word):
+        result = []
+        for chunk in phrase.split(' '):
+            parsed_chunk = self.morph.parse(chunk)
+            if chunk.lower() == master_word.word.lower():
+                infl = {case}
+                inflected = master_word.inflect(infl)
+                if inflected:
+                    result.append(inflected.word)
+                else:
+                    logger.error(u'Can not inflect word {1}: {0}'.format(phrase, chunk))
+                    result.append(chunk)
+                continue
+
+            dependent = None
+            for version in parsed_chunk:
+                if version.tag.POS in (u'ADJF', u'PRTF') and version.tag.case == master_word.tag.case:
+                    infl = {case, master_word.tag.number, master_word.tag.gender}
+                    if case == u'accs' and master_word.tag.gender == u'masc':
+                        infl.add(master_word.tag.animacy)
+
+                    try:
+                        inflected = version.inflect(infl)
+                    except ValueError,e:
+                        logger.error(u'{0} at {1}'.format(e, version.word))
+                        dependent = version
+                    else:
+                        if inflected:
+                            dependent = inflected
+                        else:
+                            logger.error(u'Can not inflect word {1}: {0}'.format(phrase, version.word))
+                            dependent = version
+                    break
+
+            if dependent:
+                result.append(dependent.word)
+            else:
+                result.append(chunk)
+
+        return u' '.join(result)
+
+    def _inflect_without_master(self, case, phrase):
+        result = []
+        for chunk in phrase.split(' '):
+            parsed = self.parse_first(chunk)
+            if parsed:
+                if any(map(lambda x: x in parsed.tag, ({'NOUN', 'nomn'}, {'ADJF', 'nomn'}))):
+                    infl = {case}
+                    if case == u'accs':
+                        infl.add('inan')
+                    try:
+                        inflected = parsed.inflect(infl)
+                        if inflected:
+                            result.append(inflected.word)
+                        else:
+                            logger.error(u'Can not inflect word {1}: {0}'.format(phrase, chunk))
+                            result.append(chunk)
+                    except ValueError:
+                        result.append(chunk)
+                else:
+                    result.append(chunk)
+            else:
+                result.append(chunk)
+
+        return u' '.join(result)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Inflect input phrase')
+    parser.add_argument('word', help='Phrase to inflect')
+    parser.add_argument('case', help='case from https://pymorphy2.readthedocs.org/en/latest/user/grammemes.html',
+                        choices=('nomn', 'gent', 'datv', 'accs', 'ablt','loct', 'voct', 'gen2', 'acc2', 'loc2'))
+    parser.add_argument('-v', '--verbose', type=int, required=False, default=0, choices=LOGGING_LEVELS)
+    args = parser.parse_args()
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=LOGGING_LEVELS[args.verbose])
+
+    morph = pymorphy2.MorphAnalyzer()
+    inflector = PhraseInflector(morph)
+    print inflector.inflect(args.word.decode('utf8'), args.case)
---
+++ test.py
@@ -0,0 +1,61 @@
+# coding=utf8
+import unittest
+import pymorphy2
+from inflect import PhraseInflector
+
+
+class InflectTestSuite(unittest.TestCase):
+
+    def setUp(self):
+        self.inflector = PhraseInflector(pymorphy2.MorphAnalyzer())
+
+    def test_select_master_word(self):
+        phrases = (
+            (u'шар для боулинга', u'шар'),
+            (u'красный шар для боулинга', u'шар'),
+            (u'красный шар', u'шар'),
+            (u'красная лента', u'лента'),
+            (u'Кирпич керамический лицевой полнотелый одинарный велюровый 1НФ М150', u'кирпич'),
+            (u'Труба стальная профильная электросварная', u'труба'),
+            (u'плита для стальных перекрытий бетонная', u'плита'),
+        )
+        for phrase, master_word in phrases:
+            parsed = self.inflector.select_master(phrase.split(' '))
+            self.assertEqual(parsed.parsed.word, master_word, u'{0} instead {1}'.format(parsed.parsed.word, master_word))
+
+    def test_inflection_with_master_genitive(self):
+        phrases = (
+            (u'шар для боулинга', u'шара для боулинга'),
+            (u'красный шар для боулинга', u'красного шара для боулинга'),
+            (u'красный шар', u'красного шара'),
+            (u'красная лента', u'красной ленты'),
+            (u'кирпич керамический лицевой полнотелый одинарный велюровый 1НФ М150',
+                u'кирпича керамического лицевого полнотелого одинарного велюрового 1НФ М150'),
+            (u'труба стальная профильная электросварная', u'трубы стальной профильной электросварной'),
+            (u'плита для стальных перекрытий бетонная', u'плиты для стальных перекрытий бетонной'),
+        )
+        for phrase, result in phrases:
+            master = self.inflector.select_master(phrase.split(' '))
+            genitive = self.inflector._inflect_with_master('gent', phrase, master.parsed)
+            self.assertEqual(genitive, result)
+
+
+    def test_inflection_with_master_accusative(self):
+        phrases = (
+            (u'шар для боулинга', u'шар для боулинга'),
+            (u'красный шар для боулинга', u'красный шар для боулинга'),
+            (u'красный шар', u'красный шар'),
+            (u'красная лента', u'красную ленту'),
+            (u'кирпич керамический лицевой полнотелый одинарный велюровый 1НФ М150',
+                u'кирпич керамический лицевой полнотелый одинарный велюровый 1НФ М150'),
+            (u'труба стальная профильная электросварная', u'трубу стальную профильную электросварную'),
+            (u'плита для стальных перекрытий бетонная', u'плиту для стальных перекрытий бетонную'),
+            (u'моя прекрасная няня', u'мою прекрасную няню'),
+        )
+        for phrase, result in phrases:
+            master = self.inflector.select_master(phrase.split(' '))
+            accusative = self.inflector._inflect_with_master('accs', phrase, master.parsed)
+            self.assertEqual(accusative, result)
+
+if __name__ == '__main__':
+    unittest.main()
