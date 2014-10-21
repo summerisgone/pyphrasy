ecf3521153420dd875f3878d3e6b53217b9d6c37
Юрий Пайков <paykov@findmeals.com>
2014-10-21 20:34:34 +0600

    fixed app.py and fixed description

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import json
 import pymorphy2
 from flask import Flask, request, render_template
-from inflect import PhraseInflector, CASE_CHOICES
+from inflect import PhraseInflector, GRAM_CHOICES
 
 app = Flask(__name__)
 app.debug = True
@@ -20,18 +20,19 @@ def inflect():
 
     if 'phrase' not in params:
         return u'укажите слово', 400,  {'Content-Type':'text/plain; charset=utf-8'}
-    if 'cases' not in params:
-        return u'выберите падежи', 400,  {'Content-Type':'text/plain; charset=utf-8'}
+    if 'forms' not in params and 'cases' not in params :
+        return u'выберите падежи или/и числа', 400,  {'Content-Type':'text/plain; charset=utf-8'}
 
     phrase = params['phrase']
-    cases = params.getlist('cases')
-    cases = set(CASE_CHOICES).intersection(cases)
+    form_sets = params.getlist('forms') if params.getlist('forms') else params.getlist('cases')
+
 
     morph = pymorphy2.MorphAnalyzer()
     inflector = PhraseInflector(morph)
     result = {'orig': phrase}
-    for case in cases:
-        result[case] = inflector.inflect(phrase, case)
+    for forms_string in form_sets:
+        form_set = set(forms_string.split(',')) & set(GRAM_CHOICES)
+        result[forms_string] = inflector.inflect(phrase, form_set)
     return json.dumps(result), 200, {'Content-Type':'text/json; charset=utf-8'}
 
 if __name__ == "__main__":
